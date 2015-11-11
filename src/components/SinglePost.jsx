5a15e58b98eedd4244b3b405e74ba602c8251308
Luis Teixeira <luisteixeira@MAC18.home>
2015-11-11 00:10:21 +0000

    debug single page

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -3,6 +3,8 @@ import PostsStore from '../stores/PostsStore.jsx'
 import { RouteHandler , Lifecycle , State ,RouteContext, History } from 'react-router'
 import Helmet from "react-helmet";
 import ImageLoader from './ImageLoader'
+import LazyLoad from 'react-lazy-load'
+
 
 
 const SinglePost = React.createClass({
@@ -67,22 +69,52 @@ const SinglePost = React.createClass({
   render() {
     let fetchedData = this.state.fetchedData;
     let acf = fetchedData.acf;
-
-    console.log(fetchedData);
+//    console.log(acf);
     return (
       <article className="SinglePost">
         <Helmet title={fetchedData.title.rendered+' | '+ appConfig.wordpressName} />
         {
           this.state.isVisible ?
           <div className='container'>
-            <ImageLoader imageSrc={acf.imagem_banner.sizes.banner_image}/>
+            { acf.imagem_banner ? <ImageLoader imageSrc={acf.imagem_banner.sizes.banner_image}/> : <div/> }
 
-            <div className='stagger'>
+            <div className='stagger col-md-8 col-md-push-2'>
               <h2 className="the-title text-center text-uppercase margin-t-60">{fetchedData.title.rendered}</h2>
-              <div className="the-content col-md-8 col-md-push-2 margin-t-60" dangerouslySetInnerHTML={{__html: fetchedData.content.rendered }} />
 
+              <div className="the-content margin-t-60" dangerouslySetInnerHTML={{__html: fetchedData.content.rendered }} />
+              <div className="the-link text-center margin-t-40">
+                <a href={acf.link_url} title={acf.link_label}>{acf.link_label}</a>
+              </div>
+
+              <div className="the-role-wapper margin-t-40">
+                <div className="the-role-title uppercase"><strong>ROLE</strong></div>
+                <div className="the-role-loop ">
+                {
+
+                    acf.role ? acf.role.map(function(role , i){
+                      let roleKey = 'role-'+i
+                      return <div key={roleKey} dangerouslySetInnerHTML={{__html: role.content }} />
+                    }) : ''
+
+                }
+                </div>
+              </div>
+            </div>
+            <div className="loop-images margin-t-60 margin-b-60">
+
+               {
+                 acf.images ? acf.images.map(function(image , i){
+                   let imageKey = 'image-'+i;
+                   return (
+                      <LazyLoad  key={imageKey} once ref="LazyLoad">
+                        <ImageLoader className="project-image" imageSrc={image.image.sizes.large}/>
+                      </LazyLoad>
+                  )
+                 }) : ''
+               }
 
             </div>
+
           </div>
           : <div />
         }
